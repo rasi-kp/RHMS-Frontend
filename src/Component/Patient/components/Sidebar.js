ca631c262f7a4dc2e16d8b013a72ee9e0c443aa9
Rasi K P <rasir239@gmail.com>
2024-05-18 01:10:28 +0530

    solve slidebar problem

--- src/Component/Patient/components/Sidebar.js
+++ src/Component/Patient/components/Sidebar.js
@@ -16,7 +16,7 @@ import { ToastContainer, toast } from 'react-toastify';
 
 
 const Sidebar = ({ isOpen, toggle }) => {
-  const location=useLocation()
+  const location = useLocation()
   const [activeLink, setActiveLink] = useState(null);
   const navigate = useNavigate()
   const dispatch = useDispatch();
@@ -44,16 +44,57 @@ const Sidebar = ({ isOpen, toggle }) => {
         <AiOutlineClose className='text-black w-5 h-5 ml-8 cursor-pointer md:hidden' onClick={toggle} />
       </div>
       <ul className="text-[#7F8F98]">
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><AiFillDashboard className="w-5 h-5 mr-3 " /><span className="cursor-pointer text-sm text-md" onClick={() => handleItemClick('/patient')}><Link to="/patient">Dashboard</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/members' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><FaUsers className="w-5 h-6 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/members')}><Link to="/patient/members">Members</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/appointments'|| activeLink === '/patient/appointments/complete' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><IoDocuments className="w-5 h-5 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/appointments')}><Link to="/patient/appointments"> Appointment</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/doctors' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><FaUserDoctor className="w-5 h-5 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/doctors')}><Link to="/patient/doctors">Doctors</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/messages' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><RiMessage2Fill className="w-5 h-5 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/messages')}><Link to="/patient/messages">Messages</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/online' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><IoMdVideocam className="w-5 h-5 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/online')}><Link to="">Online Consultant</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/ambulance' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><BiSolidAmbulance className="w-5 h-5 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/ambulance')}><Link to="">Ambulance</Link></span></li>
-        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/subscription' ? 'bg-blue-100 text-[#3497F9]' : ''}`}><MdSubscriptions className="w-5 h-5 mr-3" /><span className="cursor-pointer text-sm" onClick={() => handleItemClick('/patient/subscription')}><Link to="">Subscription</Link></span></li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient')}>
+          <Link to="/patient" className="flex items-center w-full">
+            <AiFillDashboard className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm text-md">Dashboard</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/members' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/members')}>
+          <Link to="/patient/members" className="flex items-center w-full">
+            <FaUsers className="w-5 h-6 mr-3" />
+            <span className="cursor-pointer text-sm">Members</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/appointments' || activeLink === '/patient/appointments/complete' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/appointments')}>
+          <Link to="/patient/appointments" className="flex items-center w-full">
+            <IoDocuments className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm">Appointment</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/doctors' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/doctors')}>
+          <Link to="/patient/doctors" className="flex items-center w-full">
+            <FaUserDoctor className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm">Doctors</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/messages' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/messages')}>
+          <Link to="/patient/messages" className="flex items-center w-full">
+            <RiMessage2Fill className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm">Messages</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/online' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/online')}>
+          <Link to="" className="flex items-center w-full">
+            <IoMdVideocam className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm">Online Consultant</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/ambulance' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/ambulance')}>
+          <Link to="" className="flex items-center w-full">
+            <BiSolidAmbulance className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm">Ambulance</span>
+          </Link>
+        </li>
+        <li className={`pl-6 p-3 lg:pb-3 hover:text-[#3497F9] flex ${activeLink === '/patient/subscription' ? 'bg-blue-100 text-[#3497F9]' : ''}`} onClick={() => handleItemClick('/patient/subscription')}>
+          <Link to="" className="flex items-center w-full">
+            <MdSubscriptions className="w-5 h-5 mr-3" />
+            <span className="cursor-pointer text-sm">Subscription</span>
+          </Link>
+        </li>
         {/* <li className="pl-6 p-2 flex"><MdMore className="w-5 h-5 mr-3"/>Others</li> */}
       </ul>
+
 
       <div className="absolute bottom-0 pl-6 p-6 text-[#7F8F98]">
 
